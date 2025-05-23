1a3aa002add401105bfbd11a21fa294242a409ed
mmstr7 <139415943+mmstr7@users.noreply.github.com>
2025-05-23 12:17:15 +0900

    gitignore 갱신
    업로드 기본 제외 폴더 세팅/제거

--- CoopPuzzle/Intermediate/Build/Win64/x64/ShaderCompileWorker/Development/PostBuild-1.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-echo Copying C:\Program Files\Epic Games\UnrealEngine-release\Engine\Binaries\Win64\ShaderCompileWorker.exe to C:\Program Files\Epic Games\UnrealEngine-release\Engine\Binaries\Win64\XGEControlWorker.exe
-copy /Y /B "C:\Program Files\Epic Games\UnrealEngine-release\Engine\Binaries\Win64\ShaderCompileWorker.exe" /B "C:\Program Files\Epic Games\UnrealEngine-release\Engine\Binaries\Win64\XGEControlWorker.exe" >nul:
